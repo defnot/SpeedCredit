f331b85dd2d70aa8dcfe291a80fe6efe7185352f
Dany <dimitrov829@gmail.com>
2016-02-10 15:36:09 +0200

    Almost finished

--- ZenTasksJava/zentasksJava/app/controllers/SessionTracking.java
+++ ZenTasksJava/zentasksJava/app/controllers/SessionTracking.java
@@ -1,17 +1,20 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.Session;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Dany on 1/30/2016.
  */
 public class SessionTracking extends Controller {
-    @Security.Authenticated(Secured.class)
-    public static Result track() {
+    public static Result trackAll() {
         return ok(Json.toJson(Session.find.all()));
     }
 }
